ef810c420e627611a9c355e499b1b1c006fae218
PanHengyu <936187425@qq.com>
2021-12-03 19:31:03 +0800

    threadPoolv1 and debug1

--- test/threadPool_test.go
+++ test/threadPool_test.go
@@ -2,15 +2,39 @@ package test
 
 import (
 	threadPool2 "commonArchitecture/threadPool"
+	"fmt"
 	"testing"
+	"time"
 )
 
+//测试任务
+type Add struct{
+	FirstOp int
+	SecondOp int
+	Result int
+}
+func(self *Add)Do()int{
+	self.Result=self.FirstOp+self.SecondOp
+	return 0
+}
 /**
 	测试线程池
  */
-func TestTreadPool(t *testing.T){
-	threadPool:=threadPool2.NewThreadPool(100)//容量为100的线程池
+func TestThreadPool(t *testing.T){
+	threadPool:=threadPool2.NewThreadPool(3)//容量为100的线程池
 	threadPool.Run()
-	threadPool.AddJob()//
+	add:=&Add{FirstOp: 1,SecondOp: 2}
+	add1:=&Add{FirstOp: 1,SecondOp: 2}
+	add3:=&Add{FirstOp: 1,SecondOp: 2}
+	add4:=&Add{FirstOp: 1,SecondOp: 2}
+	threadPool.AddJob(add)
+	threadPool.AddJob(add1)
+	threadPool.AddJob(add3)
+	threadPool.AddJob(add4)
+	time.Sleep(1e8) //如果没有sleep,那么可能还没完成done就已经threadPool.End()
+	fmt.Println(add.Result)
+	fmt.Println(add1.Result)
+	fmt.Println(add3.Result)
+	fmt.Println(add4.Result)
 	threadPool.End()
 }
